71d3f23c17c669f234ba05466a182c9ab99f7584
Alex-P83 <topweb1983@gmail.com>
2020-11-13 18:48:32 +0200

    service slide

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,16 @@ $(function(){
                 items:1
             }
         }
-    }) 
+    }); 
+
+  //Click
+
+  var widthTitle = document.querySelector('.services_02 .title h6');
+
+  $('.services_02 .arrow').on('click',function(){
+    $('.inner_block').slideToggle();
+    $('.services_02_back').toggleClass('active');
+  })
+  
 });
 
