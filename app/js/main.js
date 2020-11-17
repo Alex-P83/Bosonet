398679e992ef7b66369fdcebffb60491a9df5d43
Alex-P83 <topweb1983@gmail.com>
2020-11-17 18:59:06 +0200

    continuation adaprive

--- app/js/main.js
+++ app/js/main.js
@@ -43,7 +43,41 @@ $(function(){
         }
     }); 
     
+    let team_carousel = $('.services_02 .team_tiles');
+    let team_settings = {
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  };
 
+  if(window.innerWidth < 992){
+      team_carousel.owlCarousel(team_settings);
+  }
+  
+  //Resize slider
+  $(window).resize(function (e) {
+    if (window.innerWidth < 768) {
+      if ($(pc__slider).hasClass('slick-initialized')) {
+        $(pc__slider).slick('unslick');
+      }
+    }
+    else {
+        if(!$(pc__slider).hasClass('slick-initialized')){
+          $(pc__slider).slick(pc__slider__settings);
+      }    
+    }
+  });
   //Click
 
   var widthTitle = document.querySelector('.services_02 .title h6');
@@ -52,6 +86,41 @@ $(function(){
     $('.inner_block').slideToggle();
     $('.services_02_back').toggleClass('active');
   })
+
+
+  
+
   
 });
 
+ // Menu button
+ (function() {
+    "use strict";
+    var toggles = document.querySelectorAll(".c-hamburger");
+    for (var i = toggles.length - 1; i >= 0; i--) {
+      var toggle = toggles[i];
+      toggleHandler(toggle);
+    };
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        e.preventDefault();
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      });
+    }
+  })();
+
+    ///Drop menu
+    $(function(){
+        $('.colapse-menu-button .c-hamburger').on('click',function(){
+          $('header .menu-wrapper').slideToggle();
+        });
+      });
+      
+      // Hidden menu
+      var menu = $('header .menu-wrapper'); 
+      $(window).resize(function(){
+          var wid = $(window).width();
+          if(wid > 570 && menu.is(':hidden')) {
+              menu.removeAttr('style');      
+          }
+      });  
